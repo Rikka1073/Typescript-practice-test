49a6163a3d2c3319e71560f99e58fdc6fe29c35d
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-09-05 22:42:43 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Table, TableCaption, TableContainer, Tbody, Td, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { GetAllTodo } from "./lib/todo";
+import { Todo } from "./domain/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  useEffect(() => {
+    const getAlltodos = async () => {
+      const todosData = await GetAllTodo();
+      console.log(todosData);
+      setTodos(todosData);
+    };
+
+    getAlltodos();
+  }, []);
   return (
     <>
-      <Button colorScheme="teal">ボタン</Button>
+      <TableContainer>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Title</Th>
+              <Th>Done</Th>
+              <Th>created_at</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {todos.map((todo) => {
+              return (
+                <Tr key={todo.id}>
+                  <Td>{todo.title}</Td>
+                  <Td>{todo.done ? "TRUE" : "FALSE"}</Td>
+                  <Td>{todo.created_at}</Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
